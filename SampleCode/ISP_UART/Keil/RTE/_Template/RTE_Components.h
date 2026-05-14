5d9a36fbbbf76084ceb91cea5703935f669374a9
Released <released.chen@gmail.com>
2026-05-14 19:34:43 +0800

    update

---
+++ SampleCode/ISP_UART/Keil/RTE/_Template/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ISP_UART' 
+ * Target:  'Template' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "M253.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
